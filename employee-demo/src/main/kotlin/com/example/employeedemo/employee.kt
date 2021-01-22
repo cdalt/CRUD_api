9b2f5ae809aed6cfcea3276e90282da2d1255c5b
Dalton Natale <cdalton.natale@gmail.com>
2021-01-21 19:46:04 -0700

    updated controller

--- employee-demo/src/main/kotlin/com/example/employeedemo/employee.kt
+++ employee-demo/src/main/kotlin/com/example/employeedemo/employee.kt
@@ -1,14 +1,3 @@
 package com.example.employeedemo;
 
-data class Greeting(val name: String, val salary: Integer, val married: boolean)
-
-@RestController
-class GreetingController {
-
-    val counter = AtomicLong()
-
-    @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
-
-}
+data class Employee (val name: String, val salary: Integer, val married: Boolean)
